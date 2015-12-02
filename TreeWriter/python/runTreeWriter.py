3edc8a253ffa8e69c685ec8ad6539d13cb19a1ff
Knut Kiesel <kiesel@cern.ch>
2015-12-02 10:18:40 +0100

    updating global tags, see https://twiki.cern.ch/twiki/bin/view/CMS/JECDataMC

--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -50,9 +50,9 @@ gtName = "auto:run2_data" if isRealData else "auto:run2_mc"
 
 # for further global tags, see here:
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMiniAOD
-if re.match( "/.*/Run2015.*05Oct2015.*/MINIAOD", options.dataset ): gtName = "74X_dataRun2_reMiniAOD_v0"
-if re.match( "/.*/Run2015D-PromptReco-v4/MINIAOD", options.dataset ): gtName = "74X_dataRun2_Prompt_v4"
-if re.match( "/.*/.*RunIISpring15MiniAODv2.*/MINIAODSIM", options.dataset ): gtName = "74X_mcRun2_asymptotic_v2"
+if re.match( "/.*/Run2015.*05Oct2015.*/MINIAOD", options.dataset ) \
+    or re.match( "/.*/Run2015D-PromptReco-v4/MINIAOD", options.dataset ): gtName = "74X_dataRun2_v5"
+if re.match( "/.*/.*RunIISpring15MiniAODv2.*/MINIAODSIM", options.dataset ): gtName = "74X_mcRun2_asymptotic_v4"
 process.GlobalTag = GlobalTag(process.GlobalTag, gtName, '')
 
 hardPUveto=True if options.dataset.startswith("/QCD_HT100to200") else False
