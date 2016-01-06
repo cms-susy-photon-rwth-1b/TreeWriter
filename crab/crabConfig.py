e7a20d6925aa708e15ea54ff6b6c5b58643507f3
Johannes Lange <johannes.lange@rwth-aachen.de>
2016-01-06 13:20:32 +0100

    v05

--- crab/crabConfig.py
+++ crab/crabConfig.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
         config.Data.outputDatasetTag = 'V07'
         config.Data.outLFNDirBase = "/store/user/kiesel/13TeV/nTuples/"
     elif user=="lange":
-        config.Data.outputDatasetTag = 'v04'
+        config.Data.outputDatasetTag = 'v05'
         config.Data.outLFNDirBase = "/store/user/jolange/run2/"
     elif user=="rmeyer":
         config.Data.outputDatasetTag = 'V01'
