71ab05a0854a4e94083c85add3fcb55cf2e8ca44
Johannes Lange <johannes.lange@rwth-aachen.de>
2016-02-11 08:34:15 +0100

    valid database path when submitting with crab

--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -43,6 +43,10 @@ options.fastSim = (options.fastSim
                    or bool(re.match( "/SMS-.*/.*/USER", options.dataset )) # signal scan
                    )
 
+# where .db files are placed (e.g. for JEC, JER)
+localDataBasePath=('sqlite_file:src/TreeWriter/TreeWriter/data/' if isCrabSubmission
+                   else 'sqlite_file:'+cmssw_src+'/TreeWriter/TreeWriter/data/')
+
 # the actual TreeWriter module
 process = cms.Process("TreeWriter")
 
@@ -114,8 +118,8 @@ process.jec = cms.ESSource("PoolDBESSource",
                                    label  = cms.untracked.string('AK4PFchs')
                                ),
                            ),
-                           connect = (cms.string('sqlite_file:'+cmssw_src+'/TreeWriter/TreeWriter/data/Summer15_25nsV7_DATA.db') if isRealData
-                                      else cms.string('sqlite_file:'+cmssw_src+'/TreeWriter/TreeWriter/data/Summer15_25nsV7_MC.db'))
+                           connect = (cms.string(localDataBasePath+'Summer15_25nsV7_DATA.db') if isRealData
+                                      else cms.string(localDataBasePath+'Summer15_25nsV7_MC.db'))
 )
 ## add an es_prefer statement to resolve a possible conflict from simultaneous connection to a global tag
 process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
@@ -168,7 +172,7 @@ process.jer = cms.ESSource("PoolDBESSource",
                                    label  = cms.untracked.string('AK4PFchs')
                                ),
                            ),
-                           connect = cms.string('sqlite_file:'+cmssw_src+'/TreeWriter/TreeWriter/data/Summer15_25nsV6.db')
+                           connect = cms.string(localDataBasePath+'Summer15_25nsV6.db')
 )
 process.es_prefer_jer = cms.ESPrefer('PoolDBESSource', 'jer')
 
