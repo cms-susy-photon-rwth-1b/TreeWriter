7ce732040a0606e046dad4481e3607c65b619828
Knut Kiesel <kiesel@cern.ch>
2016-03-31 10:37:47 +0200

    updated knuts dy sample

--- crab/crabConfig.py
+++ crab/crabConfig.py
@@ -93,7 +93,7 @@ datasets["kiesel"] = [
     '/JetHT/Run2015C_25ns-16Dec2015-v1/MINIAOD',
     '/JetHT/Run2015D-16Dec2015-v1/MINIAOD',
     '/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM',
-    '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
+    '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM',
     '/SingleElectron/Run2015C_25ns-16Dec2015-v1/MINIAOD',
     '/SingleElectron/Run2015D-16Dec2015-v1/MINIAOD',
     '/SMS-T5Wg_mGl-1550_mNLSP-0to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-FastAsympt25ns_74X_mcRun2_asymptotic_v2-v1/MINIAODSIM',
