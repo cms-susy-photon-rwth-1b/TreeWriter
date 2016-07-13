2540595b34945fe2bcd903c7bde65d5b0d93e7aa
Johannes Lange <johannes.lange@rwth-aachen.de>
2016-07-13 17:51:27 +0200

    pt 100 cut
    Conflicts:
    	TreeWriter/python/runTreeWriter.py

--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -198,7 +198,7 @@ if user=="kiesel":
 
 elif user=="lange" or user=="jschulz":
     process.TreeWriter.jet_pT_cut=30.
-    process.TreeWriter.photon_pT_cut=50
+    process.TreeWriter.photon_pT_cut=100
     process.TreeWriter.storeTriggerObjects=False
     process.TreeWriter.triggerNames=[
         "HLT_Photon90_CaloIdL_PFHT500_v",
