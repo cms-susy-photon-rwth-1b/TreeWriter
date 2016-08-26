4ff305fe250cc4e578b6520f5500aef7765e4d0a
Knut Kiesel <kiesel@cern.ch>
2016-08-26 07:09:08 +0200

    changing global tag

--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -46,12 +46,11 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 
 # determine global tag
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-gtName="76X_dataRun2_16Dec2015_v0" if isRealData else "76X_mcRun2_asymptotic_RunIIFall15DR76_v1"
-if "74X" in dataset and not isRealData: "74X_mcRun2_asymptotic_v5"
-process.GlobalTag = GlobalTag(process.GlobalTag, gtName, '')
-
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+if isRealData:
+    process.GlobalTag.globaltag = "80X_dataRun2_Prompt_ICHEP16JEC_v0"
+else:
+    process.GlobalTag.globaltag = "80X_mcRun2_asymptotic_2016_miniAODv2_v1"
 
 ######################
 # PHOTONS, ELECTRONS #
