d21149c34a0ab1b87944ea279a0e1d973c6d62c9
Knut Kiesel <kiesel@cern.ch>
2016-11-28 08:42:18 +0100

    adding johannes local file for testing

--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -28,6 +28,7 @@ options.register ('user',
                   "Name the user. If not set by crab, this script will determine it.")
 
 # defaults
+#options.inputFiles = 'root:///user/jschulz/CMSSW_8_0_12/src/TreeWriter/tools/ZNuNuGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph_PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1_MINIAODSIM.root'
 options.inputFiles = 'root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/00000/264A540A-571A-E611-8C5E-0025904E3FCE.root'
 options.inputFiles = 'root://cms-xrd-global.cern.ch//store/mc/RunIISpring16MiniAODv2/SMS-T5Wg_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/80000/F227DD10-813E-E611-A722-6C3BE5B5C460.root'
 options.inputFiles = 'root://cms-xrd-global.cern.ch//store/data/Run2016G/SinglePhoton/MINIAOD/PromptReco-v1/000/278/969/00000/CE30EEA0-9666-E611-AB4F-02163E014411.root'
