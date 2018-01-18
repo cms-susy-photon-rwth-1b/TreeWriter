39e6be8d3329178491546f8eb9a11ef3110e4b51
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2018-01-18 11:31:53 +0100

    Added working mass selection for new GGM Scan

--- TreeWriter/plugins/TreeWriter.cpp
+++ TreeWriter/plugins/TreeWriter.cpp
@@ -1125,12 +1125,19 @@ void TreeWriter::beginLuminosityBlock(edm::LuminosityBlock const& iLumi, edm::Ev
    signal_m2_ = 0;
    if (gen_header.isValid()) {
       modelName_ = gen_header->configDescription();
+      std::cout<<modelName_<<std::endl;
       std::smatch sm;
       if (regex_match(modelName_, sm, std::regex(".*_(\\d+)_(\\d+)"))) {
          signal_m1_ = std::stoi(sm[1]);
          signal_m2_ = std::stoi(sm[2]);
       } else if (regex_match(modelName_, sm, std::regex(".*_(\\d+)"))) {
          signal_m1_ = std::stoi(sm[1]);
+      } else if (regex_match(modelName_, sm, std::regex(".*_M1(\\d+)_M3(\\d+)"))) {
+         signal_m1_ = std::stoi(sm[1]);
+         signal_m2_ = std::stoi(sm[2]);
+      } else if (regex_match(modelName_, sm, std::regex(".*_M1(\\d+)_M2(\\d+)"))) {
+         signal_m1_ = std::stoi(sm[1]);
+         signal_m2_ = std::stoi(sm[2]);
       }
    }
 
